41f47438324e8134f0649bf5162c97a12184ffa8
Heitor Ferrari <heitorferrari9@gmail.com>
2026-05-11 11:18:20 -0300

    feat: redis Db

--- .app/routes.py
+++ .app/routes.py
@@ -6,22 +6,22 @@ router = APIRouter()
 
 class PerguntaRequest(BaseModel):
     session_id: str
-    pergunta:   str
+    ask:   str
 
 class RespostaResponse(BaseModel):
-    resposta: str
+    ans: str
 
 @router.post("/perguntar", response_model=RespostaResponse)
 async def perguntar(body: PerguntaRequest):
-    if not body.pergunta.strip():
+    if not body.ask.strip():
         raise HTTPException(status_code=400, detail="A pergunta nao pode ser vazia!")
 
     if not body.session_id.strip():
         raise HTTPException(status_code=400, detail="O ID da sessao nao pode estar vazio!")
 
-    resposta = await buscar_resposta(body.session_id, body.pergunta)
+    ans = await buscar_resposta(body.session_id, body.ask)
 
-    if resposta is None:
+    if ans is None:
         raise HTTPException(status_code=404, detail="Não encontrei informações suficientes na base")
 
-    return RespostaResponse(resposta=resposta)
+    return RespostaResponse(ans=ans)
